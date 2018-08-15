084ffe4696bd56007bb8d3213285a59e0fb2c920
Christopher Weber <chrisrrweber@gmail.com>
2018-08-15 22:36:05 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -23,11 +23,16 @@
 # end
 
 def speak_to_grandma(phrase)
-  if phrase != phrase.upcase
-    return "HUH! SPEAK UP, SONNY"
-  elsif phrase === "I LOVE YOU GRANDMA"
+  if phrase === "I LOVE YOU GRANDMA"
     return "I LOVE YOU TOO PUMPKIN"
+  elsif phrase != phrase.upcase
+    return "HUH! SPEAK UP, SONNY"
   else phrase === phrase.upcase
     return "NO, NOT SINCE 1938!"
   end
 end
+
+if phrase === "I LOVE YOU GRANDMA"
+    return "I LOVE YOU TOO PUMPKIN"
+elsif phrase != phrase.upcase
+    return "HUH! SPEAK UP, SONNY"
